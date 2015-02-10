0774f974e02150ea95b6b0c134996a831ca9c9e9
nculture <tonculture@hotmail.com>
2015-02-10 16:43:02 +0800

    simplePyTry
    py file

---
+++ roading/2015-2-2/PyPlugin.py
@@ -0,0 +1,10 @@
+def HelloWorld(strName):
+        print "Hello", strName
+
+def AddMult(a, b):
+	print "in FunctionAddMult..."
+	print a, b
+	return a + b, a * b
+
+if __name__ == "__main__":
+        print "1"
